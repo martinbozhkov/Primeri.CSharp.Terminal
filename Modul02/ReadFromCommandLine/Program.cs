39d4294971fec6da461fe941e08ac1e55ae237b0
Мартин Божков <martin.bozhkov@mail.bg>
2016-11-30 22:07:57 +0200

    'Модул 02, Видео 06 - Работа със стрингове

--- Modul02/ReadFromCommandLine/Program.cs
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -9,6 +9,12 @@ namespace ReadFromCommandLine
 			//Дефиниране на променливи
 
 			int a = 0, b = 0;
+			int c = 0;
+			string first = "Hello";
+			string second = "World";
+			object one = first + " " + second;
+			string fourth = "0";
+
 
 			Console.WriteLine("След като въведете число, натиснете Enter." + "\n\n");
 
@@ -24,10 +30,41 @@ namespace ReadFromCommandLine
 
 			Console.WriteLine("");
 
+		
 			//Печат на резултат
 
 			Console.WriteLine("Резултата a + b e: " + (a + b).ToString () + "\n\n\n");
 
+			//Въвеждане на входни параметри
+
+			Console.Write("Моля въведете една дума:  ");
+			fourth = (Console.ReadLine());
+
+			Console.WriteLine("");
+
+			Console.Write("Моля въведете едно число:  ");
+			c = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("");
+				
+			//Печат на резултат
+
+			Console.WriteLine("Резултата от конкатенацията на думата и числото e : " + (fourth + " " + c) + "\n\n");
+
+			//Основни операции със стрингове
+
+			string test = "1";
+			test += ", 2";
+			test += ", 100";
+			test += ", 4";
+			test += ", 5";
+
+			Console.WriteLine("Събиране с += :  " + test + "\n");
+
+			test = test.Replace(", ", ";");
+			Console.WriteLine("Работа с Replace :  " + test + "\n");
+
+			Console.WriteLine("3ти знак: " + test.Split (';')[2] + "\n\n\n");
 
 		}
 	}
