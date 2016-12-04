d3d6bd16d09c15cb2febf8f3990ce0334c8a8c4b
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-04 22:14:19 +0200

    Модул 02, Видео 09 - Деклариране и използване на Enum

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -4,6 +4,9 @@ namespace TableProject
 {
 	class MainClass
 	{
+
+		enum ti {ime = 0, familia, godini };
+
 		public static void Main(string[] args)
 		{
 			//Едномерни масиви. Функции
@@ -31,16 +34,16 @@ namespace TableProject
 			string[,] table = new string[2, 3];
 
 			//Въвеждане на първи ред
-			table[0, 0] = "Мартин"; table[0, 1] = "Симеонов"; table[0, 2] = "31";
-
+			table[0, (int) ti.ime] = "Мартин"; table[0, (int) ti.familia] = "Симеонов"; table[0, (int) ti.godini] = "31";
+	
 			//Въвеждане на втори ред
-			table[1, 0] = "Симеон"; table[1, 1] = "Мартинов"; table[1, 2] = "41";
+			table[1, (int) ti.ime] = "Симеон"; table[1, (int) ti.familia] = "Мартинов"; table[1, (int) ti.godini] = "41";
 
 			//Печат
 			Console.Write("Кой ред искате да видите: ");
 			int _index = Convert.ToInt32(Console.ReadLine()) - 1;
 
-			Console.WriteLine("\n" + (table[_index, 0])[0] + ". " + table[_index, 1] + ", " + table[_index, 2] + "г.\n\n");
+			Console.WriteLine("\n" + (table[_index, (int) ti.ime])[0] + ". " + table[_index, (int) ti.familia] + ", " + table[_index, (int) ti.godini] + "г.\n\n");
 		}
 	}
 }
