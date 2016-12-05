3c65e880c74f29e2d1de0f2d54dc7a043e9022ba
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-05 22:41:25 +0200

    Модул 03, Видео 02 - Форматиране на дробни числа

---
+++ Modul03/Modul03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modul03
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double a = 0, b = 0;
+
+			Console.Write("Моля въведете a =  "); a = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Моля въведете b =  "); b = Convert.ToDouble(Console.ReadLine());
+
+			string _format = "C2";
+
+			Console.WriteLine("\n\nРазликата a / b =  " + (a / b).ToString (_format) + "\n\n");
+		}
+	}
+}
