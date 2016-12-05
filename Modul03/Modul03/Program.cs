fa9df1a131915a9acfde7e10c572227875a74e06
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-05 23:28:39 +0200

    Модул 03, Видео 03 - Математически операции

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -6,14 +6,28 @@ namespace Modul03
 	{
 		public static void Main(string[] args)
 		{
-			double a = 0, b = 0;
+			double a = 0, b = 0, c = 0;
 
 			Console.Write("Моля въведете a =  "); a = Convert.ToDouble(Console.ReadLine());
 			Console.Write("Моля въведете b =  "); b = Convert.ToDouble(Console.ReadLine());
 
-			string _format = "C2";
+			//Форматиране с дименсия
+			string _format = "#0.00";
+			Console.WriteLine("\nРазликата a / b =  " + (a / b).ToString("C2") + "\n\n");
 
-			Console.WriteLine("\n\nРазликата a / b =  " + (a / b).ToString (_format) + "\n\n");
+			//Степенуване
+			c = Math.Pow (a, b);
+			Console.WriteLine("a ^ b =  " + c.ToString(_format) + "\n");
+
+			//Коренуване
+			c = Math.Sqrt ( b );
+			Console.WriteLine("b ^ 1/2 =  " + c.ToString(_format) + "\n");
+
+			//Закръгления
+			c = Math.Round (a / b) ;
+			Console.WriteLine("Разликата a / b =  " + c.ToString() + "\n\n");
+
+
 		}
 	}
 }
