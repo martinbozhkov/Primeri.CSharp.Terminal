039f233ec9abfe76eea6056a9f460fbc8a7d4cd5
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-08 17:51:29 +0200

    Модул 03, Видео 06 - Работа с условни оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -15,29 +15,43 @@ namespace BoolExplore
 			//Функции на bool
 			int[] _i = new int[] { 4, 3, 2 };
 
-			Console.Write("Моля въведете число: ");
+			Console.Write("Моля въведете индекс на число: ");
 			int _ui = Convert.ToInt32(Console.ReadLine());
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+			//bool _check01 = _ui <= _i.Length;
+			//bool _check02 = _ui > 0;
 
-			//Console.WriteLine(_check01);
-			//Console.WriteLine(_check02);
-			//Console.WriteLine(_i [_ui - 1]);
+			////Console.WriteLine(_check01);
+			////Console.WriteLine(_check02);
+			////Console.WriteLine(_i [_ui - 1]);
 
-			//Логически функции на bool
+			////Логически функции на bool
 
-			bool _exp01 = _check01 && _check02;		// Логическо И "&&"
-			bool _exp02 = _check01 || _check02;		// Логическо ИЛИ "||"
-			bool _exp03 = ! _check01;				// Отрицание "!"
-
-
-			Console.WriteLine("\nЛогическо И: " + _exp01.ToString ());
-			Console.WriteLine("\nЛогическо ИЛИ: " + _exp02.ToString());			
-			Console.WriteLine("\nОтрицание на " + _check01.ToString() + ": " + _exp03 + "\n\n");
+			//bool _exp01 = _check01 && _check02;		// Логическо И "&&"
+			//bool _exp02 = _check01 || _check02;		// Логическо ИЛИ "||"
+			//bool _exp03 = ! _check01;				// Отрицание "!"
 
 
+			//Console.WriteLine("\nЛогическо И: " + _exp01.ToString ());
+			//Console.WriteLine("\nЛогическо ИЛИ: " + _exp02.ToString());			
+			//Console.WriteLine("\nОтрицание на " + _check01.ToString() + ": " + _exp03 + "\n\n");
 
-		}
+			if (_ui > 0 && _ui <= _i.Length)
+			{
+				Console.WriteLine("\nЕлементът с индекс " + _ui + " e: " + _i[_ui - 1] + "\n\n");
+			}
+			else {
+				if (_ui >= _i.Length)
+				{
+					Console.WriteLine("\nИндекса е прекалено голям!\n\n\n ");
+				}
+				if (_ui <= 0)
+				{
+					Console.WriteLine("\nИндекса е прекалено малък!\n\n\n ");
+				}
+			}
+
+
+			}
 	}
 }
