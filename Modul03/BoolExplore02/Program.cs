bbe5136f5439481a995ee2a2b78c77fa89e8dcaf
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-11 16:17:56 +0200

    Модул 03, Видео 08 - Използване на условно присвояване

---
+++ Modul03/BoolExplore02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BoolExplore02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int[] _i = new int[] { 4, 3, 2 };
+			int _ui = 0;
+
+			Console.Write("Моля въведете индекс на число: ");
+			string _input = Console.ReadLine();
+
+			bool _check01 = int.TryParse(_input, out _ui);
+			_ui = (_ui <= _i.Length) ? _ui: 3;
+			_ui = (_ui > 0) ? _ui : 1;
+
+			if (_check01)
+			{
+				Console.WriteLine("\nЕлементът от масива e: " + _i[_ui - 1] + "\n\n");
+			}
+
+		}
+	}
+}
