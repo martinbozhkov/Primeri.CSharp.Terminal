d8f0f4d0c0c3af801161f3b182fda2aadcc7aaf7
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-11 16:49:44 +0200

    Модул 03, Видео 09 - Използване на оператор Switch

--- Modul03/BoolExplore02/Program.cs
+++ Modul03/BoolExplore02/Program.cs
@@ -13,14 +13,40 @@ namespace BoolExplore02
 			string _input = Console.ReadLine();
 
 			bool _check01 = int.TryParse(_input, out _ui);
-			_ui = (_ui <= _i.Length) ? _ui: 3;
-			_ui = (_ui > 0) ? _ui : 1;
+			//_ui = (_ui <= _i.Length) ? _ui: 3;
+			//_ui = (_ui > 0) ? _ui : 1;
 
-			if (_check01)
-			{
-				Console.WriteLine("\nЕлементът от масива e: " + _i[_ui - 1] + "\n\n");
+			//if (_check01)
+			//{
+			//	Console.WriteLine("\nЕлементът от масива e: " + _i[_ui - 1] + "\n\n");
+			//}
+
+			switch (_ui)
+			{   
+				case 1:
+					{
+						Console.Write("\n\n1ви елемент от масива е: ");
+						Console.WriteLine(_i[_ui - 1] + "\n\n");
+						break;
+					}
+				case 2:
+					{
+						Console.Write("\n\n2ри елемент от масива е: ");
+						Console.WriteLine(_i[_ui - 1] + "\n\n");
+						break;
+					}
+				case 3:
+					{
+						Console.Write("\n\n3ти елемент от масива е: ");
+						Console.WriteLine(_i[_ui - 1] + "\n\n");
+						break;
+					}
+				default:
+					{
+						Console.Write("\n\nВ масива няма стойност за този индекс. \nОпитайте с индекс между 1 и 3! \n\n\n");
+						break;
+					}
 			}
-
-		}
+			}
 	}
 }
