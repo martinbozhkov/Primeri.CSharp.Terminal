0b203c4c803435541c993a2894ea00567b165147
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-13 21:27:53 +0200

    Модул 04, Видео 04-05 - Цикъл While и do ... while

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -21,16 +21,47 @@ namespace Modul04
 			Console.WriteLine("=================================");
 
 			//Тяло на таблица
-			for (int i = 0; i < _table.Length / 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					Console.Write(_table[i, j].PadRight(12));
-				}
-				Console.WriteLine("");
-			}
-			Console.WriteLine("\n\n");
 
+			//Пример с For
+
+			//for (int i = 0; i < _table.Length / 3; i++)
+			//{
+			//	for (int j = 0; j < 3; j++)
+			//	{
+			//		Console.Write(_table[i, j].PadRight(12));
+			//	}
+			//	Console.WriteLine("");
+			//}
+			//Console.WriteLine("\n\n");
+
+
+			//Пример с While
+
+			//int i = 0;
+			//while (i < _table.Length / 3)
+			//{
+			//	Console.WriteLine(
+			//		_table[i, 0].PadRight(12) +
+			//		_table[i, 1].PadRight(12) +
+			//		_table[i, 2].PadRight(12));
+			//	i++;
+			//}
+			//Console.WriteLine("\n\n");
+
+
+			//Пример с do ... while
+			int i = 0;
+
+			do
+			{
+				Console.WriteLine(
+					_table[i, 0].PadRight(12) +
+					_table[i, 1].PadRight(12) +
+					_table[i, 2].PadRight(12));
+				i++;
+			} while (i < _table.Length / 3);
+
+			Console.WriteLine("\n\n");
 
 		}
 	}
